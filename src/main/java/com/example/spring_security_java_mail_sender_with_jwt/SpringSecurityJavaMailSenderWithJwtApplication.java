78e182bdf173ecfde61cf18c830a8885464fb9e2
manoj <kannamanoj2@gmail.com>
2022-08-16 20:16:14 +0530

    Initial commit

---
+++ src/main/java/com/example/spring_security_java_mail_sender_with_jwt/SpringSecurityJavaMailSenderWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_java_mail_sender_with_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJavaMailSenderWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJavaMailSenderWithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_security_java_mail_sender_with_jwt/SpringSecurityJavaMailSenderWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_java_mail_sender_with_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJavaMailSenderWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
